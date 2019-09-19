5d311039b91ff4513c5a776fb5f48f8a6659f46d
Iain Diamond <iain.diamond@gmail.com>
2019-09-19 11:57:40 +0100

    Carve out SocialMediaButtons component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import React from 'react'
-import {
-  TwitterShareButton,
-  TumblrShareButton,
-  TwitterIcon,
-  TumblrIcon,
-} from 'react-share'
 import './App.css'
+import SocialMediaButtons from './components/SocialMediaButtons'
 
 class App extends React.Component {
   index = 0
@@ -28,7 +23,6 @@ class App extends React.Component {
       author,
       quote,
     })
-
     this.index = this.index + 1
     if (this.index >= this.quotes.length) {
       await this.loadQuotes()
@@ -38,6 +32,7 @@ class App extends React.Component {
   async loadQuotes() {
     const quoteURL = 'https://favqs.com/api/quotes'
     const accessKey = 'Token token=a7cdfe2bcd1b4cef333643b0498da614'
+
     let data = await fetch(quoteURL, {
       method: 'GET',
       headers: {
@@ -53,8 +48,6 @@ class App extends React.Component {
     if (!quote) {
       return <div>Loading Quotes ...</div>
     }
-    const text = `${'"' + quote + '" ' + author}`
-    const iconSize = 36
     return (
       <div className="App" id="quote-box">
         <div id="text">
@@ -64,31 +57,7 @@ class App extends React.Component {
         </div>
         <div id="author">{author}</div>
         <div id="buttons">
-          <div id="social-media">
-            <TwitterShareButton
-              url={'https://favqs.com'}
-              title={text}
-              className="twitter-button"
-            >
-              <TwitterIcon size={iconSize} round />
-            </TwitterShareButton>
-            <TumblrShareButton
-              url={'https://favqs.com'}
-              title={'Favourite Quotes'}
-              caption={text}
-              className="tumblr-button"
-              tags={['quotes']}
-            >
-              <TumblrIcon size={iconSize} round />
-            </TumblrShareButton>
-            <a
-              Style="display: none;"
-              href="https://twitter.com/intent/tweet"
-              id="tweet-quote"
-            >
-              Only needed to pass the tests
-            </a>
-          </div>
+          <SocialMediaButtons props={this.state} />
           <button id="new-quote" onClick={this.displayQuote}>
             New Quote
           </button>
---
+++ src/components/SocialMediaButtons.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import {
+  TwitterShareButton,
+  TumblrShareButton,
+  TwitterIcon,
+  TumblrIcon,
+} from 'react-share'
+
+const SocialMediaButtons = ({ quote, author }) => {
+  const text = `${'"' + quote + '" ' + author}`
+  const iconSize = 36
+  return (
+    <div id="social-media">
+      <TwitterShareButton
+        url={'https://favqs.com'}
+        title={text}
+        className="twitter-button"
+      >
+        <TwitterIcon size={iconSize} round />
+      </TwitterShareButton>
+      <TumblrShareButton
+        url={'https://favqs.com'}
+        title={'Favourite Quotes'}
+        caption={text}
+        className="tumblr-button"
+        tags={['quotes']}
+      >
+        <TumblrIcon size={iconSize} round />
+      </TumblrShareButton>
+      <a
+        Style="display: none;"
+        href="https://twitter.com/intent/tweet"
+        id="tweet-quote"
+      >
+        Only needed to pass the tests
+      </a>
+    </div>
+  )
+}
+
+export default SocialMediaButtons
